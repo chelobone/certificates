6e9dd1ea4f233cfeb238f2ed67eb8f7a90e752e0
Marcelo Friz Gonzalez <46089516+chelobone@users.noreply.github.com>
2024-06-25 15:17:38 -0400

    Update CreateCertificate.ps1

--- CreateCertificate.ps1
+++ CreateCertificate.ps1
@@ -1,12 +1,12 @@
 #$CertificatePassword="Imag3nCDDAc0n3x2022"
 #$CertificatePassword="C0llahuas1CDDInt3gr@c1on3s2022"
-$CertificatePassword="C0lbun.Int3gr@ci0nPV.2022"
-$Tenant="fc9f30fb-f690-4c42-bbc6-d99cb95218fa"
+$CertificatePassword=""
+$Tenant=""
 $CertificateOutDir = ".\Certificates"
 if (-not ( Test-Path -Path $CertificateOutDir -PathType Container )) {
 	md $CertificateOutDir
 }
-$app = Register-PnPAzureADApp -ApplicationName "CertQAPortalVirtual" -Tenant $Tenant -OutPath $CertificateOutDir `
+$app = Register-PnPAzureADApp -ApplicationName "" -Tenant $Tenant -OutPath $CertificateOutDir `
 	-CertificatePassword (ConvertTo-SecureString -String $CertificatePassword -AsPlainText -Force) `
 	-Scopes "SPO.Sites.FullControl.All" `
 	-Store CurrentUser -DeviceLogin
