a90ea8a45b73ab3c8babe6eed142b168f5a2da9e
Marcelo Friz Gonzalez <46089516+chelobone@users.noreply.github.com>
2024-06-25 12:48:12 -0400

    Update CreateCertificatev2.ps1

--- CreateCertificatev2.ps1
+++ CreateCertificatev2.ps1
@@ -1,5 +1,5 @@
-$certname = "CertQAPortalVirtual"    ## Replace {certificateName}
+$certname = "{certName}"    ## Replace {certificateName}
 $cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
-Export-Certificate -Cert $cert -FilePath "C:\Azure\Functions\Imagen.CDD.Functions\Certificates\$certname.cer"   ## Specify your preferred location
-$mypwd = ConvertTo-SecureString -String "C0lbun.Int3gr@ci0nPV.2022" -Force -AsPlainText  ## Replace {myPassword}
-Export-PfxCertificate -Cert $cert -FilePath "C:\Azure\Functions\Imagen.CDD.Functions\Certificates\$certname.pfx" -Password $mypwd   ## Specify your preferred location
+Export-Certificate -Cert $cert -FilePath "C:\Certificates\$certname.cer"   ## Specify your preferred location
+$mypwd = ConvertTo-SecureString -String "{password}" -Force -AsPlainText  ## Replace {myPassword}
+Export-PfxCertificate -Cert $cert -FilePath "C:\Certificates\$certname.pfx" -Password $mypwd   ## Specify your preferred location
